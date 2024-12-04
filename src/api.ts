b23f636d8df8b74c3eb80f3e5c93a8d7efb0e676
Jacques Vella Critien <vellacritien@gmail.com>
2024-12-04 12:40:25 +0100

    Fixing some types

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Response } from "express";
+import { Request, NextFunction, Response, RequestHandler } from "express";
 import { getRemovedTransactionsSince, getTransactionsSince } from "./lib/db";
 import { API_SECRET } from "./config/config";
 
@@ -6,40 +6,40 @@ import express from "express";
 const apiRouter = express.Router();
 
 // Middleware to check API key
-const checkApiKey = (req: { headers: { [x: string]: string | undefined; }; }, res: Response, next: NextFunction) => {
+const checkApiKey: RequestHandler = (req, res, next) => {
     const apiKey = req.headers['api-key'] as string | undefined; // Get the 'api-key' from the request headers
   
     // Check if the 'api-key' is correct
     if (apiKey === API_SECRET) {
-      return next();
+        next();
     } else {
-      return res.status(403).json({ message: 'Forbidden: Invalid API Key' }); // If invalid, send a 403 response
+        res.status(403).json({ message: 'Forbidden: Invalid API Key' }); // If invalid, send a 403 response
     }
   };
 
-apiRouter.get('/txs', checkApiKey, async (req: { query: { since: any; }; }, res: Response) => {
-    let since = req.query.since
-    if(isNaN(since)){
+apiRouter.get('/txs', checkApiKey, async (req: Request, res: Response) => {
+    let since = req.query.since as string
+    if(isNaN(Number(since))){
         res.status(400).json({
             success: false,
             message: "Since parameter must be a number"
         })
     }
 
-    let txs = await getTransactionsSince(since)
+    let txs = await getTransactionsSince(Number(since))
     res.status(200).json(txs)
 });
 
-apiRouter.get('/reorgedTxs', checkApiKey, async (req: { query: { since: any; }; }, res: Response) => {
-    let since = req.query.since
-    if(isNaN(since)){
+apiRouter.get('/reorgedTxs', checkApiKey, async (req: Request, res: Response) => {
+    let since = req.query.since as string
+    if(isNaN(Number(since))){
         res.status(400).json({
             success: false,
             message: "Since parameter must be a number"
         })
     }
 
-    let txs = await getRemovedTransactionsSince(since)
+    let txs = await getRemovedTransactionsSince(Number(since))
     res.status(200).json(txs)
 });
 
